578476dbdfb1da014d0cfbe09f807b73e69397e1
Divya Rai <thinkaheadapps@gmail.com>
2024-06-06 13:38:15 +0530

    Button n event handling

--- MyUI5App/webapp/controller/App.controller.js
+++ MyUI5App/webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+	"sap/ui/core/mvc/Controller",
+	 "sap/m/MessageToast"
+],function (Controller, MessageToast) {
+    "use strict";
 
-	return Controller.extend("Demo1.MyUI5App.controller.App", {
-		onInit: function () {
-
-		}
-	});
+    return Controller.extend("Demo1.MyUI5App.controller.App", {
+        onPress: function () {
+            MessageToast.show("Button Pressed");
+        }
+    });
 });
